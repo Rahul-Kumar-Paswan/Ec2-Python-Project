b5106d0c3f5acf9aff2de9e73a63e583869f3d49
Rahul-Kumar-Paswan <123447842+Rahul-Kumar-Paswan@users.noreply.github.com>
2023-07-05 20:55:14 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -13,9 +13,9 @@ def buildImage() {
 
 def deployApp() {
     echo 'deploying the application...'
-    def dockerCmd = 'docker run -d --name rahul-project -p 3000:3000 rahulkumarpaswan/my-python-project:1.3'
+    def dockerCmd = 'docker run -d --name ishu-project -p 3000:3000 rahulkumarpaswan/my-python-project:1.3'
     sshagent(['ec2-user-Rahul']) {
-    sh "ssh -o StrictHostKeyChecking=no ec2-user@13.232.90.226 ${dockerCmd}"
+    sh "ssh -o StrictHostKeyChecking=no ec2-user@15.206.72.171 ${dockerCmd}"
     }
 } 
 
