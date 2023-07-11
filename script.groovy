60b88db19f315ff39ca7d5703a808e36746516ba
Rahul-Kumar-Paswan <123447842+Rahul-Kumar-Paswan@users.noreply.github.com>
2023-07-11 21:18:17 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,17 +5,17 @@ def check() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t rahulkumarpaswan/my-python-project:1.3 .'
+        sh 'docker build -t rahulkumarpaswan/my-python-project:1.4 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push rahulkumarpaswan/my-python-project:1.3'
+        sh 'docker push rahulkumarpaswan/my-python-project:1.4'
     }
 } 
 
 def deployApp() {
     echo 'deploying the application...'
-    def dockerCmd = 'docker run -d --name ishu-project -p 3000:3000 rahulkumarpaswan/my-python-project:1.3'
+    def dockerCmd = 'docker run -d --name ishu-project-1 -p 3000:3000 rahulkumarpaswan/my-python-project:1.4'
     sshagent(['ec2-user-Rahul']) {
-    sh "ssh -o StrictHostKeyChecking=no ec2-user@15.206.72.171 ${dockerCmd}"
+    sh "ssh -o StrictHostKeyChecking=no ec2-user@3.109.181.27  ${dockerCmd}"
     }
 } 
 
